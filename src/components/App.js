3af20ccd02d15751307c1757d06c97086022d610
Harshita222 <kumariharsita501@gmail.com>
2024-07-06 19:57:39 +0530

    moo

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import store from './store';
-import LoremIpsum from './components/LoremIpsum';
+import store from '../store';
+import LoremIpsum from '../components/LoremIpsum';
 
 const App = () => {
   return (
